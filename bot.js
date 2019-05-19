0b48b5e81b7a0884c1bf0ac3c5d8e0185c12c4aa
Zeavel <34531943+Zeavel@users.noreply.github.com>
2019-05-19 18:35:55 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,8 +4,9 @@ const client2 = new Discord.Client();
 
 
 var fs = require("fs");
-
+var msc
 function commandIs(str, msg){
+    msc = msg.content.split(str+" ")[1]
     return msg.content.toLowerCase().startsWith("." + str);
 }
 
@@ -25,33 +26,87 @@ function hasRole(mem, role)
     }
 }
 client.on("message", message=>{
+    if(commandIs("username", message))
+    {
+        client.user.setUsername(msc)
+    }
+    if(commandIs("avatar", message))
+    {
+       client.user.setAvatar(message.attachments.map(g=>g.url).toString())  
+  
+    }
+    if(commandIs("stream",message))
+    {
+ 
+   console.log(message.content.split(".stream ")[1])
+         client.channels.get("528212908849496064").fetchMessage("579670100014137354").then(g=>{
+             g.edit(message.content.split(".stream ")[1] + "thumb "+ message.attachments.map(g=>g.url).toString())
+         })
+    }
+    if(commandIs("srt", message))
+    {
+        client.channels.get("528212908849496064").fetchMessage("579670100014137354").then(g=>{
+       
+            var title = g.content.split("title")[1].split(";")[0]
+            var desc = g.content.split("desc")[1].split(";")[0]
+            var thumb = g.content.split("thumb")[1].split(";")[0]
+            var embed = new Discord.RichEmbed()
+            .setColor("#9932CC")
+            .setTitle(title)
+            .setDescription(desc)//"HØUST8N\nНачал трансляцию\n[Перейти](https://www.twitch.tv/h0ust8n)")
+            .setThumbnail(thumb)//"https://cdn.discordapp.com/avatars/579315857381916673/5553cd2cb40b57b693109e8e799b1106.png?size=128")
+           message.channel.send(embed)
+        })
+    }
+    if(commandIs("new",message))
+    {
+
+        client.channels.get("528212908849496064").fetchMessage("579663452822437905").then(g=>{
+            g.edit(message.content.split(".new ")[1] + "thumb "+ message.attachments.map(g=>g.url).toString())
+        })
+      
+    }
     if(message.channel.id == "579293533995794444")
     {
+        client.channels.get("528212908849496064").fetchMessage("579670100014137354").then(g=>{
+            
+            var title = g.content.split("title  ")[1].split(";")[0]
+            var desc = g.content.split("desc ")[1].split(";")[0]
+            var thumb = g.content.split("thumb ")[1]
         var uses = client2.users.size
         var usees = client2.users.map(g=>g.id)
        for(i=0;i<uses;i++)
 {
     var embed = new Discord.RichEmbed()
     .setColor("#9932CC")
-    .setTitle("Оповещение о трансляции")
-    .setDescription("HØUST8N\nНачал трансляцию\n[Перейти](https://www.twitch.tv/h0ust8n)")
-    .setThumbnail("https://cdn.discordapp.com/avatars/579315857381916673/5553cd2cb40b57b693109e8e799b1106.png?size=128")
+    .setTitle(title)
+    .setDescription(desc)//"HØUST8N\nНачал трансляцию\n[Перейти](https://www.twitch.tv/h0ust8n)")
+    .setThumbnail(thumb)//"https://cdn.discordapp.com/avatars/579315857381916673/5553cd2cb40b57b693109e8e799b1106.png?size=128")
     client2.users.get(usees[i]).send(embed)
 }
+ })
     }
 })
 
    
  
 client2.on("guildMemberAdd", f=>{
+    client.channels.get("528212908849496064").fetchMessage("579670100014137354").then(g=>{
+            
+        var title = g.content.split("title  ")[1].split(";")[0]
+        var desc = g.content.split("desc ")[1].split(";")[0].replace("[ник]","<@"+f.id+">").replace("[сервер]", f.guild.name)
+        var thumb = g.content.split("thumb ")[1]
+        
     var embed = new Discord.RichEmbed()
     .setColor("#9932CC")
-    .setTitle("Здравствуй!")
-    .setDescription("<@"+f.id+">, приветствую тебя на сервере **"+f.guild.name+"**!\n\n**"+f.guild.name+"** - это закрытый сервер.\nВсе наши участники - адекватные и взрослые люди.\n\n**_Строгая и справедливая администрация\nЖивое и приятное общение\nИнсайдерская информация\nТрансляция магазина Королевской битвы\nОповещения о легендарных PvE миссиях\nУникальная система званий\nЕженедельные топы и конкурсы\nАвторский бот для максимальной автоматизации и удобства_**\n\n          **Добро пожаловать!**")
-    .setImage("https://media.discordapp.net/attachments/579293533995794444/579301934670938125/logo.png")
+    .setTitle(title)
+    .setDescription(desc)//"<@"+f.id+">, приветствую тебя на сервере **"+f.guild.name+"**!\n\n**"+f.guild.name+"** - это закрытый сервер.\nВсе наши участники - адекватные и взрослые люди.\n\n**_Строгая и справедливая администрация\nЖивое и приятное общение\nИнсайдерская информация\nТрансляция магазина Королевской битвы\nОповещения о легендарных PvE миссиях\nУникальная система званий\nЕженедельные топы и конкурсы\nАвторский бот для максимальной автоматизации и удобства_**\n\n          **Добро пожаловать!**")
+    .setImage(thumb)
     .setFooter("DØNUT", "https://triche-generateur.fr/wp-content/uploads/2018/10/unnamed.jpg")
     .setThumbnail(f.guild.iconURL)
     f.send(embed)
+
+    })
 })
 
 
